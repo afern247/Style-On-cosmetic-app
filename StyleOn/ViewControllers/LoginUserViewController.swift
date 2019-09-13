a6c2a9988ba869dbcbf33596780f2119483c7119
Ramses <machadoramses@yahoo.com>
2019-09-13 14:30:48 -0400

    Sign in - authentication style

--- StyleOn/ViewControllers/LoginUserViewController.swift
+++ StyleOn/ViewControllers/LoginUserViewController.swift
@@ -11,21 +11,21 @@ import FirebaseAuth
 
 class LoginUserViewController: UIViewController {
 
-    
     @IBOutlet var emailTextField: UITextField!
-    
     @IBOutlet var passwordTextField: UITextField!
-    
     @IBOutlet var loginButtonRounded: UIButton!
-    
     @IBOutlet var errorLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        errorLabel.alpha = 0
+        
         self.loginButtonRounded.layer.cornerRadius = 20.0
         self.loginButtonRounded.layer.borderWidth = 1.0
         self.loginButtonRounded.layer.borderColor = UIColor.white.cgColor
+        
         
         super.viewDidLoad()
         let topColor = UIColor(red: 162/255, green: 21/255, blue: 255/255, alpha: 1)
@@ -40,44 +40,44 @@ class LoginUserViewController: UIViewController {
         
         gradientLayer.frame = self.view.bounds
         self.view.layer.insertSublayer(gradientLayer, at: 0)
-
         // Do any additional setup after loading the view.
-        setUpElements()
     }
     
-
-    func setUpElements(){
-        errorLabel.alpha = 0
-    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    @IBAction func loginTapped(_ sender: Any) {
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    @IBAction func loginButtonTapped(_ sender: Any) {
         
+        
+        // TODO: Validate Text Fields
+        
+        // Create cleaned versions of the text field
         let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        Auth.auth().signIn(withEmail: email, password: password) {(result, error) in
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
             if error != nil {
-                
+                // Couldn't sign in
                 self.errorLabel.text = error!.localizedDescription
                 self.errorLabel.alpha = 1
-                //Cuden't sign in
             }
             else {
-                
                 let salonViewController = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.salonViewController) as? SalonViewController
                 
                 self.view.window?.rootViewController = salonViewController
                 self.view.window?.makeKeyAndVisible()
+
             }
+        }
     }
     
-}
 }
