64d80b638576bf4273cd0ec9b3e63d49af244efb
Ramses <machadoramses@yahoo.com>
2019-09-13 12:16:02 -0400

    Sign in - authentication

--- StyleOn/ViewControllers/LoginUserViewController.swift
+++ StyleOn/ViewControllers/LoginUserViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginUserViewController: UIViewController {
 
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginButtonRounded: UIButton!
+    
+    @IBOutlet var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.loginButtonRounded.layer.cornerRadius = 20.0
+        self.loginButtonRounded.layer.borderWidth = 1.0
+        self.loginButtonRounded.layer.borderColor = UIColor.white.cgColor
+        
+        super.viewDidLoad()
+        let topColor = UIColor(red: 162/255, green: 21/255, blue: 255/255, alpha: 1)
+        let bottomColor = UIColor(red: 0/255, green: 194/255, blue: 189/255, alpha: 1)
+        
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 0.5]
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
 
+    func setUpElements(){
+        errorLabel.alpha = 0
+    }
     /*
     // MARK: - Navigation
 
@@ -26,5 +58,26 @@ class LoginUserViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        Auth.auth().signIn(withEmail: email, password: password) {(result, error) in
+            if error != nil {
+                
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+                //Cuden't sign in
+            }
+            else {
+                
+                let salonViewController = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.salonViewController) as? SalonViewController
+                
+                self.view.window?.rootViewController = salonViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+    }
+    
+}
 }
