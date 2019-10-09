570d1455902ef6d5e203f5831f0bce53f9df3a6a
Ramses <machadoramses@yahoo.com>
2019-10-09 10:25:14 -0400

    Post Item Page for user Styler to show services (bug found)

--- StyleOn/ViewControllers/PostServiceStylerViewController.swift
+++ StyleOn/ViewControllers/PostServiceStylerViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseDatabase
 
 class PostServiceStylerViewController: UITabBarController {
 
+    @IBOutlet var postImageView: UIImageView!
+    @IBOutlet var captionTextView: UITextView!
+    var textViewPlaceholderText = "Tell us about your service"
+    
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +26,18 @@ class PostServiceStylerViewController: UITabBarController {
     }
     
 
+    @IBAction func postDidTap(_ sender: Any) {
+        
+        if captionTextView.text != textViewPlaceholderText && captionTextView.text != "" && selectedImage != nil {
+            let newPost = Post(image: self.selectedImage, caption: self.captionTextView.text)
+            newPost.save()
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    @IBAction func cancelDidTap(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
@@ -27,4 +48,38 @@ class PostServiceStylerViewController: UITabBarController {
     }
     */
 
+}
+extension PostServiceStylerViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        var selectedImage: UIImage?
+        if let editedImage = info[.editedImage] as? UIImage {
+            selectedImage = editedImage
+            self.postImageView.image = selectedImage!
+            picker.dismiss(animated: true, completion: nil)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            selectedImage = originalImage
+            self.postImageView.image = selectedImage!
+            picker.dismiss(animated: true, completion: nil)
+        }
+
+    }
+}
+
+extension PostServiceStylerViewController {
+    
+    func uploadImage(_ image:UIImage, completion: @escaping (_ url: URL?) -> ()){
+        let storageRef = Storage.storage().reference().child("myimage.png")
+        let imgData = postImageView.image?.pngData()
+        let metaData = storageMetadata()
+        metaData.contentType = "image/png"
+        storageRef.putData(imageData!, metadata: metaData) { (metadata, error) in
+            if error == nil{
+                print("success")
+                storageRef.downloadUrl(completion: { (url, error) in completion(url!)
+                })
+            }
+        }
+    }
 }
