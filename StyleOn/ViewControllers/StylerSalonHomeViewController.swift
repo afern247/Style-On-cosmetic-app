56346186b4678b55a4a8d963906ba6e69fdeaf32
Ramses <machadoramses@yahoo.com>
2019-10-14 16:25:51 -0400

    Coded Added to neewsfeed

--- StyleOn/ViewControllers/StylerSalonHomeViewController.swift
+++ StyleOn/ViewControllers/StylerSalonHomeViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class StylerSalonHomeViewController: UIViewController {
 
+//    
+//    var posts = [Post]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        
+//        Database.database().reference().child("posts").observe(.childAdded) { (snapshot) in
+//            // snapshot is now a dictionary
+//            let newPost = Post(snapshot: snapshot)
+//            DispatchQueue.main.async {
+//                self.posts.insert(newPost, at: 0)
+//                let indexPath = IndexPath(row: 0, section: 0)
+//                self.tableView.insertRows(at: [indexPath], with: .top)
+//            }
+//        }
         // Do any additional setup after loading the view.
     }
     
@@ -26,5 +38,20 @@ class StylerSalonHomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return posts.count
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoTableViewCell
+//        let post = self.posts[indexPath.row]
+//        
+//        cell.post = post
+//
+//        return cell
+//    }
 }
