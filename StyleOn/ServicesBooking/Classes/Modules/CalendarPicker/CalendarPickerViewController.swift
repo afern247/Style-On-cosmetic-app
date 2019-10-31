ef7b21a1fecd7babdc7b2065e8bce8992f1b3da2
Ramses <machadoramses@yahoo.com>
2019-10-31 16:51:15 -0400

    Coordinates

---
+++ StyleOn/ServicesBooking/Classes/Modules/CalendarPicker/CalendarPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarPickerViewController.swift
+//  StyleOn
+//
+//  Created by Ramses Machado on 9/20/19.
+//  Copyright © 2019 Ramses Machado. All rights reserved.
+//
+
+import UIKit
+
+class CalendarPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StyleOn/ServicesBooking/Classes/Modules/CalendarPicker/Cells/CalendarPickerCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarPickerCollectionViewCell.swift
+//  StyleOn
+//
+//  Created by Ramses Machado on 9/20/19.
+//  Copyright © 2019 Ramses Machado. All rights reserved.
+//
+
+import UIKit
+
+class CalendarPickerCollectionViewCell: JTAppleCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
