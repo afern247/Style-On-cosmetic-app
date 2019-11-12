4a2040d4d97dba57d93621b063cdffd4c0b1f77f
Arturo <afern247@gmail.com>
2019-11-11 20:54:53 -0500

    ✅ Everything fixed, almost ready ✅

---
+++ StyleOn/ViewControllers/FeaturedPostsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class FeaturedPostsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var featuredImage: UIImageView!
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbDesc: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- StyleOn/ViewControllers/UserSalonViewController.swift
+++ StyleOn/ViewControllers/UserSalonViewController.swift
@@ -3,13 +3,18 @@ import Firebase
 import FirebaseAuth
 import Foundation
 
-class UserSalonViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class UserSalonViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
     
     // This is the view to display all posts from all Authors
     @IBOutlet var allPostsTableView: UITableView!
-
+    @IBOutlet weak var featuredPostsCollectionView: UICollectionView!
+    
     
     var posts = [UserPosts]()
+    let productCollectionViewId = "FeaturedPostsCollectionViewCell"
+    
+    var products = [FeaturedPosts]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +29,21 @@ class UserSalonViewController: UIViewController, UITableViewDelegate, UITableVie
         observe()
         
         allPostsTableView.reloadData()
+        
+        // Register cell
+        let nibCell = UINib(nibName: productCollectionViewId, bundle: nil)
+        featuredPostsCollectionView.register(nibCell, forCellWithReuseIdentifier: productCollectionViewId)
+        
+        
+//         init data
+        for _ in 1...25 {
+            let product = FeaturedPosts()
+            product?.Name = ""
+            product?.desc = ""
+            products.append(product!)
+        }
+        
+        featuredPostsCollectionView.reloadData()
         
     }
     
@@ -41,6 +61,7 @@ class UserSalonViewController: UIViewController, UITableViewDelegate, UITableVie
     //                           let timestamp = data["timestamp"] as? Double,
                             let first_name = data["Author"] as? String,
                             let postTitle = data["title"] as? String,
+                            let post_address = data["Address"] as? String,
                             let postDescription = data["description"] as? String,
                             let postUrl = data["postUrl"] as? String,
                             let url = URL(string:postUrl)
@@ -49,7 +70,7 @@ class UserSalonViewController: UIViewController, UITableViewDelegate, UITableVie
     //                      let newDate = self.getDateFromTimeStamp(timestamp:timestamp)
 
                                 // Store variables from DB into post
-                            let post = UserPosts(author: first_name, postTitle: postTitle, postDescription: postDescription, postUrl: url, postAddress: first_name)
+                            let post = UserPosts(author: first_name, postTitle: postTitle, postDescription: postDescription, postUrl: url, postAddress: post_address)
 
                             tempPosts.append(post)
                         }
@@ -83,7 +104,57 @@ class UserSalonViewController: UIViewController, UITableViewDelegate, UITableVie
     
     
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return products.count
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 310, height: 350)
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedPostsCollectionViewCell", for: indexPath) as! FeaturedPostsCollectionViewCell
+        let product = products[indexPath.row]
+        cell.featuredImage.image = UIImage(named: "3")
+        cell.lbName.text = product.Name!
+        cell.lbDesc.text = product.desc!
+        
+        return cell
+    }
+    
+        
+    
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+//    }
+
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let collectionViewWidth = collectionView.bounds.width
+//        return CGSize(width: collectionViewWidth/3, height: collectionViewWidth/3)
+//    }
+
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 20
+//    }
+    
+    
+    
     
     
 
+    
+    
 }
+
+
+
